5c2c3957045ec11034f999828890a93333ee4694
Azizul Islam <azizultushar98@gmail.com>
2024-08-22 19:53:53 +0600

    CORS issue!

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://my-collection-app-full-stack-iota.vercel.app",
     credentials: true,
   })
 );
